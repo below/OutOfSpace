841275e51ac40334f00c09425a6d49759f66f829
Alexander von Below <alex@vonbelow.com>
2026-01-31 19:46:10 +0100

    Expose Flash and Fade

--- OutOfSpace/ToyPadService.swift
+++ OutOfSpace/ToyPadService.swift
@@ -6,9 +6,9 @@ import DimensionPad
 final class ToyPadService: ObservableObject {
     @Published private(set) var connected: Bool = false
     @Published private(set) var pads: [UInt8: PadState] = [
-        1: PadState(present: false, uid: nil, name: nil),
-        2: PadState(present: false, uid: nil, name: nil),
-        3: PadState(present: false, uid: nil, name: nil)
+        1: PadState(present: false, uid: nil, characterID: nil, name: nil),
+        2: PadState(present: false, uid: nil, characterID: nil, name: nil),
+        3: PadState(present: false, uid: nil, characterID: nil, name: nil)
     ]
     @Published private(set) var log: [String] = []
 
@@ -58,6 +58,52 @@ final class ToyPadService: ObservableObject {
                 try await pad.setColor(pad: targetPad, r: r, g: g, b: b)
             } catch {
                 appendLog("color error: \(error)")
+            }
+        }
+    }
+    
+    func flash(pad targetPad: Pad, tickOn: UInt8 = 8, tickOff: UInt8 = 8, tickCount: UInt8 = 8, r: UInt8 = 255, g: UInt8 = 255, b: UInt8 = 0) {
+        Task { @MainActor in
+            do {
+                try await pad.flash(pad: targetPad, tickOn: tickOn, tickOff: tickOff, tickCount: tickCount, r: r, g: g, b: b)
+            } catch {
+                appendLog("flash error: \(error)")
+            }
+        }
+    }
+    
+    func flashAllDemo() {
+        Task { @MainActor in
+            do {
+                let center = FlashPad(tickOn: 8, tickOff: 8, tickCount: 12, r: 255, g: 255, b: 0)
+                let left = FlashPad(tickOn: 4, tickOff: 4, tickCount: 0xFF, r: 0, g: 255, b: 255)
+                let right = FlashPad(tickOn: 12, tickOff: 12, tickCount: 6, r: 255, g: 0, b: 255)
+                try await pad.flashAll(center: center, left: left, right: right)
+            } catch {
+                appendLog("flashAll error: \(error)")
+            }
+        }
+    }
+    
+    func fade(pad targetPad: Pad, tickTime: UInt8 = 16, tickCount: UInt8 = 6, r: UInt8 = 0, g: UInt8 = 120, b: UInt8 = 255) {
+        Task { @MainActor in
+            do {
+                try await pad.fade(pad: targetPad, tickTime: tickTime, tickCount: tickCount, r: r, g: g, b: b)
+            } catch {
+                appendLog("fade error: \(error)")
+            }
+        }
+    }
+    
+    func fadeAllDemo() {
+        Task { @MainActor in
+            do {
+                let center = FadePad(tickTime: 24, tickCount: 3, r: 255, g: 0, b: 0)
+                let left = FadePad(tickTime: 12, tickCount: 0xFF, r: 0, g: 255, b: 0)
+                let right = FadePad(tickTime: 18, tickCount: 7, r: 255, g: 255, b: 255)
+                try await pad.fadeAll(center: center, left: left, right: right)
+            } catch {
+                appendLog("fadeAll error: \(error)")
             }
         }
     }
